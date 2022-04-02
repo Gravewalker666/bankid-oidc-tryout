ef00484ebaa2399cfd9f7d435c13e41091c03d51
Gravewalker666 <heshanandrews99@gmail.com>
2022-04-02 13:07:21 +0530

    Remove javax servlets and add jarkarta servlets

---
+++ src/main/java/com/example/bankidoidctryout/IndexServlet.java
@@ -0,0 +1,24 @@
+package com.example.bankidoidctryout;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/")
+public class IndexServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        response.getWriter()
+                .append("<html>")
+                .append("<head>")
+                .append("</head>")
+                .append("<body>")
+                .append("<a href=\"/login\">Sign in with BankId</a>")
+                .append("</body>")
+                .append("</html>");
+    }
+}
