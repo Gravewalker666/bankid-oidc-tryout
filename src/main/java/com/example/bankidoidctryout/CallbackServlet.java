5f44edf20a1c48d0c5ed5b06c8604800291c9bee
Gravewalker666 <heshanandrews99@gmail.com>
2022-04-03 00:25:56 +0530

    Configure the callback endpoint halfways
    Edit the callback endpoint to recieve the code and make the request to get the access tokens and redirect the users to a seperate page

--- src/main/java/com/example/bankidoidctryout/CallbackServlet.java
+++ src/main/java/com/example/bankidoidctryout/CallbackServlet.java
@@ -1,9 +1,22 @@
 package com.example.bankidoidctryout;
 
+import com.example.bankidoidctryout.utils.OIDCClient;
+import com.google.gson.JsonObject;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
 
 @WebServlet("/callback")
 public class CallbackServlet extends HttpServlet {
-
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String code = request.getParameter("code");
+        JsonObject jsonObject = OIDCClient.getInstance().makeTokenRequest(code);
+        request.getSession().setAttribute("tokenResponse", jsonObject);
+        response.sendRedirect("/");
+        // TODO: redirect user to a place and display the contents of json object
+    }
 }
--- src/main/java/com/example/bankidoidctryout/utils/OIDCClient.java
+++ src/main/java/com/example/bankidoidctryout/utils/OIDCClient.java
@@ -4,33 +4,40 @@ import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import jakarta.ws.rs.client.Client;
 import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.Entity;
+import jakarta.ws.rs.core.MultivaluedHashMap;
 import jakarta.ws.rs.core.Response;
 
 import java.io.IOException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 import java.util.Properties;
 
 public class OIDCClient {
     private static OIDCClient oidcClient;
 
-    public static OIDCClient getInstance() {
+    public static OIDCClient getInstance() throws IOException {
         if (oidcClient != null) return oidcClient;
         return new OIDCClient();
     }
 
-    private String authenticationUrl;
+    private final String authenticationUrl;
+    private final String tokenUrl;
+    private final Properties properties;
 
-    public OIDCClient() {
+    public OIDCClient() throws IOException {
+        properties = new Properties();
+        properties.load(OIDCClient.class.getResourceAsStream("/application.properties"));
+
         Client client = ClientBuilder.newClient();
         Response response = client.target(Configurations.CONFIG_URL).request().get();
         JsonObject configurations = new Gson().fromJson(response.readEntity(String.class), JsonObject.class);
         this.authenticationUrl = configurations.get("authorization_endpoint").getAsString();
+        this.tokenUrl = configurations.get("token_endpoint").getAsString();
     }
 
-    public String getAuthenticationUrl() throws IOException {
-        Properties properties = new Properties();
-        properties.load(OIDCClient.class.getResourceAsStream("/application.properties"));
+    public String getAuthenticationUrl() {
         String clientId = encode(properties.getProperty("client-id"));
         String redirectedUrl = encode(properties.getProperty("redirect-url"));
         return String.format(
@@ -40,6 +47,26 @@ public class OIDCClient {
                 "&response_type=%s",
                 authenticationUrl, clientId, "openid+profile", redirectedUrl, "code"
         );
+    }
+
+    public JsonObject makeTokenRequest(String code) {
+        String clientId = properties.getProperty("client-id");
+        String clientSecret = properties.getProperty("client-secret");
+        String redirectUrl = properties.getProperty("redirect-url");
+
+        MultivaluedHashMap<String, String> formData = new MultivaluedHashMap<>();
+        formData.add("grant-type", "authorization_code");
+        formData.add("code", code);
+        formData.add("redirect-uri", redirectUrl);
+
+        Client client = ClientBuilder.newClient();
+        Response response = client.target(tokenUrl).request().header(
+                "Authorization", "Basic " +
+                        Base64.getEncoder().encodeToString(
+                                (clientId + ":" + clientSecret).getBytes(StandardCharsets.UTF_8)
+                        )
+        ).post(Entity.form(formData));
+        return new Gson().fromJson(response.readEntity(String.class), JsonObject.class);
     }
 
     private String encode(String text) {
